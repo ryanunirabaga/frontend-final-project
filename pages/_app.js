84d23924d05ea0ee20572c10148329c1b7ad61d9
ryan.baga <ryan.baga@globe.com.ph>
2022-11-01 16:46:39 +0800

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
+import { ColorModeContextProvider } from '../Contexts/ColorMode';
 import { AuthenticationContextProvider } from '../Contexts/authentication';
 import { GetFilesProvider } from '../Contexts/firebaseStorage';
 
 function MyApp({ Component, pageProps }) {
 
   return (
+      <ColorModeContextProvider>
         <AuthenticationContextProvider>
           <GetFilesProvider>
-            <Component {...pageProps} />
+          <Component {...pageProps} />
           </GetFilesProvider>
         </AuthenticationContextProvider>
+      </ColorModeContextProvider>
   );
 }
 
